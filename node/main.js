50e0d5ac8ec9ab2c5b9ef65c54abdfd2452bbb00
NirajRasal <nirajrasal@gmail.com>
2020-09-06 19:20:51 +0530

    node js demo code

---
+++ node/main.js
@@ -0,0 +1,20 @@
+var http = require('http');
+var fs = require('fs');
+
+    function hello(req, res) 
+{
+    res.writeHead(200, {'Content-Type': 'text/html'});
+    fs.readFile('./index.html',null,function(error,data){       /* reading html file from server*/
+        if(error){
+            res.writeHead(404);
+            res.write('File not found!');
+        }
+        else{
+            res.write(data);
+        }
+        res.end();
+    });
+}
+
+
+http.createServer(hello).listen(8000); 
